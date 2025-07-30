28faba4b7ed0e0bc2bb3d5cc10d4ad8051f1a440
Tan <tanldphan@outlook.com>
2025-07-30 13:19:30 -0700

    5.5 clone

--- main/main.c
+++ main/main.c
@@ -47,7 +47,6 @@ static void fallback_dsleep(void *nothing);
 
 #define SENSOR_BATCH_INTERVAL (1000 * 5) //seconds
 #define MINIMUM_WAKE_TIME (1000 * 40) // seconds
-//#define PERI_PWR (19)
 
 // Initialize wind data
 float wind_speed = 0;
@@ -66,16 +65,6 @@ struct tm real_time = {
 void app_main(void)
 {
     rtc_ext_init(); // must initialize RTC before wifi or INT will hold LOW
-
-    // gpio_config_t io_conf = {
-    //     .pin_bit_mask = (1ULL << PERI_PWR),
-    //     .mode = GPIO_MODE_OUTPUT,
-    //     .pull_up_en = GPIO_PULLUP_ENABLE,
-    //     .pull_down_en = GPIO_PULLDOWN_DISABLE,
-    //     .intr_type = GPIO_INTR_DISABLE
-    // };
-    // gpio_config(&io_conf);
-    // gpio_set_level(PERI_PWR, 0); // turn on peri power
     if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_UNDEFINED)
     {
         ESP_LOGW(TAG_MAIN, "RTC Warning: COLD BOOT -- Setting clock");
@@ -129,6 +118,7 @@ static void fallback_dsleep(void *nothing)
     if (monitoring_task) vTaskDelete(monitoring_task);
     if (update_task)     vTaskDelete(update_task);
     if (wind_task)       vTaskDelete(wind_task);
+
     rtc_to_dsleep();
 }
 
